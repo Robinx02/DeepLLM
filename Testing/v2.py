e5eac6200effa2d8f3e47af9536210c765d58564
Robin <robinx02x@gmail.com>
2026-06-20 18:14:54 +0530

    added attention

--- Testing/v2.py
+++ Testing/v2.py
@@ -71,6 +71,31 @@ def estimate_loss():
     return out
 
 
+#implementing the attention head in this one
+class Head(nn.module):
+    """one head of self-attention"""
+
+    def __init__(self,head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd , head_size , bias =False)
+        self.query = nn.Linear(n_embd , head_size , bias =False)
+        self.value = nn.Linear(n_embd , head_size , bias =False)
+        self.register_buffer('tril' , torch.tril(torch.ones(block_size,block_size)))
+
+    def forward(self,x):
+        B, T, C = x.shape
+        k = self.key(x)   # B , T ,C
+        q = self.query(x)    #B ,T,C
+        # compute attention socres (affinities)
+        wei = q @ k.transpose(-2,-1)*C**-0.5   # B, T ,T
+        wei = wei.masked_fill(self.tril[:T,:T] == 0 , float('-inf'))
+        wei = F.softmax(wei ,dim =-1) #B ,T ,T
+        v = self.value(x)
+        out = wei @ v
+        return out
+    
+
+
 class BigramLanguageModel(nn.Module):
 
     def __init__(self, vocab_size):
