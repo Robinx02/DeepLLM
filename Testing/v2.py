2914ca0aa247998245e33edd4c73ffd607e8783a
Robin <robinx02x@gmail.com>
2026-06-20 21:12:56 +0530

    completed transformer

--- Testing/v2.py
+++ Testing/v2.py
@@ -5,14 +5,17 @@ import os
 
 #hyperparameter --------------------
 
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 #what is the maximum context length for prediction?
-max_iters =10000
-eval_interval = 300
-learning_rate = 1e-2
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 #what is the maximum context length for prediction?
+max_iters =5000
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 #-------------------------------------
 
@@ -82,6 +85,8 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embd , head_size , bias =False)
         self.register_buffer('tril' , torch.tril(torch.ones(block_size,block_size)))
 
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self,x):
         B, T, C = x.shape
         k = self.key(x)   # B , T ,C
@@ -90,6 +95,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2,-1)*C**-0.5   # B, T ,T
         wei = wei.masked_fill(self.tril[:T,:T] == 0 , float('-inf'))
         wei = F.softmax(wei ,dim =-1) #B ,T ,T
+        wei = self.dropout(wei)
         v = self.value(x)
         out = wei @ v
         return out
@@ -97,20 +103,53 @@ class Head(nn.Module):
 class MultiHeadAttention(nn.Module):
 
     def __init__(self , num_heads , head_size):
-        super().__init()
+        super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range (num_heads)])
-    
+        self.proj = nn.Linear(n_embd ,n_embd)
+        self.dropout = nn.Dropout(dropout)
     def forward(self , x):
-        return torch.cat([h(x) for h in self.heads] , dim=-1)
+        out =  torch.cat([h(x) for h in self.heads] , dim=-1)
+        out = self.dropout(self.proj(out))
+        return out 
 
-class BigramLanguageModel(nn.Module):
+class FeedForward(nn.Module):
+    
+    def __init__(self ,n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd , 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd,n_embd),  #projection 
+            nn.Dropout(dropout),
+        )
+
+    def forward(self,x):
+        return self.net(x)
+
+class Block(nn.Module):
+    # n_embd: embedding dimensions, n_head: the number of heads
+    def __init__(self , n_embd , n_head):
+        super().__init__()
+        head_size = n_embd//n_head
+        self.sa = MultiHeadAttention(n_head ,head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self,x):
+        x = x + self.sa(self.ln1(x))
+        x =x + self.ffwd(self.ln2(x))
+        return x    
+
+class TransformerModel(nn.Module):
 
     def __init__(self, vocab_size):
         super().__init__()
         #each token directly reads off the logits for the next token from the lookup table
         self.token_embedding_table = nn.Embedding(vocab_size,n_embd)
         self.positon_embedding_table = nn.Embedding(block_size , n_embd)
-        self.sa_heads = MultiHeadAttention(4,n_embd//4)  #i.e 4 heads of 8 dimensional self attention
+        self.blocks = nn.Sequential(*[Block(n_embd,n_head=n_head) for _ in range(n_layer) ])
+        self.ln_f =nn.LayerNorm(n_embd)  #final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
     
     def forward(self,idx,targets=None):
@@ -120,7 +159,8 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx)  # (B,T,C)
         pos_emb = self.positon_embedding_table(torch.arange(T,device = device))
         x = tok_emb + pos_emb
-        x = self.sa_heads(x)
+        x = self.blocks(x)  # (B ,T ,C)
+        x = self.ln_f(x)
         logits = self.lm_head(x) 
         
         
@@ -148,7 +188,7 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx,idx_next) , dim=1) # ( B ,T+1)
         return idx    
 
-model = BigramLanguageModel(vocab_size=vocab_size)
+model = TransformerModel(vocab_size=vocab_size)
 m = model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(),lr = learning_rate)
