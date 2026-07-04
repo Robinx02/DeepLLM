3a9ac7f256bcef09bfe7223e051a620ddf2dfec0
Robin <robinx02x@gmail.com>
2026-07-04 19:28:07 +0530

    sep model with positional encoding

---
+++ model.py
@@ -0,0 +1,138 @@
+import math
+import torch 
+import torch.nn as nn
+from torch.nn import functional as F
+import os
+
+#hyperparameter --------------------
+
+block_size = 256 #what is the maximum context length for prediction?
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+n_embd = 384
+n_head = 10
+n_layer = 10
+dropout = 0.2
+
+#-------------------------------------
+
+#implementing the attention head in this one
+class Head(nn.Module):
+    """one head of self-attention"""
+
+    def __init__(self,head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd , head_size , bias =False)
+        self.query = nn.Linear(n_embd , head_size , bias =False)
+        self.value = nn.Linear(n_embd , head_size , bias =False)
+        self.register_buffer('tril' , torch.tril(torch.ones(block_size,block_size)))
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self,x):
+        B, T, C = x.shape
+        k = self.key(x)   # B , T ,C
+        q = self.query(x)    #B ,T,C
+        # compute attention socres (affinities)
+        wei = q @ k.transpose(-2,-1)*C**-0.5   # B, T ,T
+        wei = wei.masked_fill(self.tril[:T,:T] == 0 , float('-inf'))
+        wei = F.softmax(wei ,dim =-1) #B ,T ,T
+        wei = self.dropout(wei)
+        v = self.value(x)
+        out = wei @ v
+        return out
+    
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self , num_heads , head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range (num_heads)])
+        self.proj = nn.Linear(n_embd ,n_embd)
+        self.dropout = nn.Dropout(dropout)
+    def forward(self , x):
+        out =  torch.cat([h(x) for h in self.heads] , dim=-1)
+        out = self.dropout(self.proj(out))
+        return out 
+
+class FeedForward(nn.Module):
+    
+    def __init__(self ,n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd , 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd,n_embd),  #projection 
+            nn.Dropout(dropout),
+        )
+
+    def forward(self,x):
+        return self.net(x)
+
+class Block(nn.Module):
+    # n_embd: embedding dimensions, n_head: the number of heads
+    def __init__(self , n_embd , n_head):
+        super().__init__()
+        head_size = n_embd//n_head
+        self.sa = MultiHeadAttention(n_head ,head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self,x):
+        x = x + self.sa(self.ln1(x))
+        x =x + self.ffwd(self.ln2(x))
+        return x    
+
+class TransformerModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        #each token directly reads off the logits for the next token from the lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size,n_embd)
+        
+        #positional encoding
+        pe = torch.zeros(block_size , n_embd)
+        position = torch.arange(0, block_size , dtype = torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0,n_embd,2).float()*(-math.log(10000.0) / n_embd))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[: , 1::2] = torch.cos(position *div_term)
+        self.register_buffer('pe' , pe)
+
+        self.blocks = nn.Sequential(*[Block(n_embd,n_head=n_head) for _ in range(n_layer) ])
+        self.ln_f =nn.LayerNorm(n_embd)  #final layer norm
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+    
+    def forward(self,idx,targets=None):
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx)  # (B,T,C)
+        pos_emb = self.pe[:T , :]
+        x = tok_emb + pos_emb
+        x = self.blocks(x)  # (B ,T ,C)
+        x = self.ln_f(x)
+        logits = self.lm_head(x) 
+        
+        
+        if targets is None:
+            loss = None
+        else:    
+            B ,T ,C = logits.shape
+            logits = logits.view(B*T , C)  #flatten out B and T
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits ,targets) 
+        return logits , loss 
+    
+    def generate(self,idx ,max_new_tokens):
+        # idx is (B,T) array of indices in the current context 
+        for _ in range(max_new_tokens):
+            #get the predictions
+            idx_cond = idx[:,-block_size:]
+            logits , loss = self(idx_cond)  #this go to the forward() of self 
+            logits = logits[:,-1,:] # becomes (B , C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits , dim=-1) # (B,C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs,num_samples=1)
+            #append sampled index to the running sequence
+            idx = torch.cat((idx,idx_next) , dim=1) # ( B ,T+1)
+        return idx    
