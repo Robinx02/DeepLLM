7b5a873ccdcf3b6626e01cf3ec416596c3c5546b
Robin <robinx02x@gmail.com>
2026-07-04 20:06:21 +0530

    nothing

--- train.py
+++ train.py
@@ -30,3 +30,38 @@ data = torch.tensor(bpe_token.encode(text) , dtype=torch.long)
 n = int(0.9*len(data))  #90% for training
 train_data = data[:n]
 val_data = data[n:]
+
+def get_batch(split):
+    #generating small batches of data of inpur x and y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data)-block_size , (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i + 1 : i + block_size +1] for i in ix])
+    x,y = x.to(device) , y.to(device)
+    return x , y
+
+
+
+@torch.no_grad()
+def estimate_loss()
+    
+    out = {}
+    model.eval()
+    for split in ['train' , 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X , Y = get_batch(split)
+            logits , loss= model(X,Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+model = TransformerModel(vocab_size = vocab_size)
+model = model.to(device)
+optimizer = torch.optim.AdamW(model.parameters() , lr = learning_rate)
+
+start_iter = 0 # default starting iteration
+
+
