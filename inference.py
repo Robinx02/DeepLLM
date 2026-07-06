8405b516402ca7cbb85b5322db9b66dfcc3a3c98
Robin <robinx02x@gmail.com>
2026-07-06 20:07:19 +0530

    inference

--- inference.py
+++ inference.py
@@ -1,2 +1,18 @@
-from model import TransformerModel
+import torch
+from torch.nn import functional as F
+
+from model import TransformerModel , block_size , device
 from BPEToken import BPE
+
+#--- paths ---
+
+weight_path = r'C:\ptoh\DeepLLM\output\checkpoint.pth'
+tokenzier_path = r'C:\ptoh\DeepLLM\output\bpe_finance.json'
+vocab_size = 8000
+
+# --- load tokenizer --- 
+
+bpe_token = BPE.load(tokenzier_path)
+
+# ---load model---
+model = TransformerModel(vocab_size=vocab_size).to(device)
